e75635a2acd995710e7053b043fdb6485f4763df
Alex Kiura <alex.kiura@andela.com>
2016-04-07 20:46:40 +0300

    [Feature] Initialize app module.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,26 @@
+"""This module configures and creates a Flask app instance."""
+
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 def create_app(config_name):
-    pass
+    """
+    Create a Flask application instance.
+
+    Args:
+        config_name (str): A configuration to use for the application.
+
+    Returns:
+        app: A Flask application instance with necessary
+        extensions initialized and bluePrints registered.
+    """
+    app = Flask(__name__)
+    db.init_app(app)
+
+    from .api_v1 import api as api_1_blueprint
+    app.register_blueprint(api_1_blueprint, url_prefix='/api/v1.0')
+
+    return app
