f173cd18c45847ecd879764064bb1cd4439a2178
Alex Kiura <alex.kiura@andela.com>
2016-04-07 20:37:22 +0300

    [Feature] Modify API confifgurations.

--- config/config.py
+++ config/config.py
@@ -1,9 +1,34 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+"""This module contains various configuration options."""
 
 
 class Config:
     """Base configuration."""
+
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
+
+class DevConfig(Config):
+    """Development configuration."""
+
     DEBUG = True
+    DATABASE_URI = 'sqlite:///dbucketlist.db'
+
+
+class TestConfig(Config):
+    """Testing configuration."""
+
     TESTING = True
+    DATABASE_URI = 'sqlite:///tbucketlist.db'
+
+
+class ProductionConfig(Config):
+    """Production configurations."""
+
+    DEBUG = False
     DATABASE_URI = 'sqlite:///bucketlist.db'
+
+config = {
+    'development': DevConfig,
+    'testing': TestConfig,
+    'Production': ProductionConfig
+}
