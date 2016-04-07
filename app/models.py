f403eeb84b32e9646b1396b4cf1e4a49e13ddcf1
Alex Kiura <alex.kiura@andela.com>
2016-04-07 14:04:57 +0300

    [Feature] Add user model

--- app/models.py
+++ app/models.py
@@ -1,10 +1,36 @@
 """Models for bucketlist API V1."""
+from app.database import Base
+from sqlalchemy import Column, Integer, String
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User:
-    """."""
+class User(Base):
+    """Defines the user table and operations allowed."""
 
-    pass
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True)
+    password_hash = Column(String(128))
+
+    @property
+    def username(self):
+        return self.username
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @property.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
 
 
 class BucketItem:
