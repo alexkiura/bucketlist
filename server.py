02a689133d416b830dfc25128db0a45242d3d2d7
Alex Kiura <alex.kiura@andela.com>
2016-04-10 23:55:39 +0300

    [Fix] Import initialized app object.

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 """This module runs the api server."""
 import os
-from app import create_app, db
+from app import flask_app, db
 from app.models import User, BucketList, BucketListItem
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
-app = create_app()  # os.getenv('FLASK_CONFIG') or 'default')
+app = flask_app
 manager = Manager(app)
 migrate = Migrate(app, db)
 
