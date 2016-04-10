2dd2e336b81a11c29f9332bc2653b4d65cab4031
Alex Kiura <alex.kiura@andela.com>
2016-04-10 23:58:18 +0300

    [Feature] Bind flask restful APi object to Flask app .

--- app/api_v1/__init__.py
+++ app/api_v1/__init__.py
@@ -1,5 +1,18 @@
 """This module creates the api BluePrint."""
 
 from flask import Blueprint
+from flask.ext.restful import Api
+from inspect import getsourcefile
+import os.path
+import sys
 
-api = Blueprint('api', __name__)
+current_path = os.path.abspath(getsourcefile(lambda: 0))
+current_dir = os.path.dirname(current_path)
+parent_dir = current_dir[:current_dir.rfind(os.path.sep)]
+
+sys.path.insert(0, parent_dir)
+from app import create_app
+
+app = create_app()
+api = Api(app)
+api_1_blueprint = Blueprint('api_1_blueprint', __name__)
