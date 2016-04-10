2f39bb41719cc283f1075cbe708df943445dae07
Alex Kiura <alex.kiura@andela.com>
2016-04-10 23:59:22 +0300

    [Feature] Add test API resource.

--- app/api_v1/resources.py
+++ app/api_v1/resources.py
@@ -1,1 +1,9 @@
 """This module contains the resources to be served on the endpoints."""
+from . import api
+from flask.ext.restful import Resource
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
