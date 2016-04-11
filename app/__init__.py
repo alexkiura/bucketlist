2afbcaa0d124b58d2bffc928b69bd9a10e61a27f
Alex Kiura <alex.kiura@andela.com>
2016-04-11 18:02:53 +0300

    [Bug] Fix config import error.

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.restful import Api
+from config.config import config
+# import ipdb; ipdb.set_trace()
 
 db = SQLAlchemy()
 
 
-def create_app():
+def create_app(config_name):
     """
     Create a Flask application instance.
 
@@ -18,9 +21,15 @@ def create_app():
         extensions initialized and bluePrints registered.
     """
     app = Flask(__name__)
+    # app.config.from_object(config[config_name])
+    app.config.from_object(config[config_name])
     db.init_app(app)
-
-    from .api_v1 import api as api_1_blueprint
-    app.register_blueprint(api_1_blueprint, url_prefix='/api/v1.0')
+    # import ipdb; ipdb.set_trace()
+    # from .api_v1 import api_v1 as api_1_blueprint
+    # app.register_blueprint(api_1_blueprint, url_prefix='/api/v1.0')
+    # app.register_blueprint(api_1_blueprint)
 
     return app
+
+flask_app = create_app('development')
+# api = Api(flask_app)
