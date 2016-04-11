cfe5f9240c2ccbb41c1dd3e0ea7b22ffddf80a09
Alex Kiura <alex.kiura@andela.com>
2016-04-11 23:40:34 +0300

    [Feature] Add marshal fields to map with models.

---
+++ app/api_v1/serializers.py
@@ -0,0 +1,24 @@
+"""This module defines the format used by marshall to map the models."""
+from flask_restful import fields
+
+bucketlistitem_serializer = {
+    'id': fields.Integer,
+    'item_name': fields.String,
+    'priority': fields.String,
+    'done': fields.Boolean,
+    'date_created': fields.DateTime,
+    'date_modified': fields.DateTime
+}
+
+bucketlist_serializer = {
+    'id': fields.Integer,
+    'list_name': fields.String,
+    'bucketlist_items': fields.Nested(bucketlistitem_serializer),
+    'created_by': fields.Integer
+}
+
+user_serializer = {
+    'id': fields.Integer,
+    'username': fields.String,
+    'bucketlists': fields.Nested(bucketlist_serializer)
+}
