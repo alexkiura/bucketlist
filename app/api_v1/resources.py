cbebd356980e1c5d579ea07a689d1dbd84c29ecc
Alex Kiura <alex.kiura@andela.com>
2016-04-11 23:45:58 +0300

    [Chore] Create BucketListApi resource.

--- app/api_v1/resources.py
+++ app/api_v1/resources.py
@@ -1,6 +1,12 @@
 """This module contains the resources to be served on the endpoints."""
-from flask.ext.restful import Resource
+from flask.ext.restful import Resource, marshal
+from app.models import BucketList
 
 class TestResource(Resource):
     def get(self):
         return {'Message': 'Welcome to my api'}
+
+
+class BucketListApi(Resource):
+    def get(self):
+        return {'bucketlists': marshal(BucketList.query.all())}
