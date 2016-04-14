026012ddaf35856205e234662fe343e438eb2842
Alex Kiura <alex.kiura@andela.com>
2016-04-14 17:22:09 +0300

    [Feature] Add testing configs.

--- config/config.py
+++ config/config.py
@@ -23,7 +23,9 @@ class TestConfig(Config):
     """Testing configuration."""
 
     TESTING = True
-    DATABASE_URI = 'sqlite:///tbucketlist.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 class ProductionConfig(Config):
