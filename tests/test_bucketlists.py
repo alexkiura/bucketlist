64439c6b8fecb5866ea3bcece9975d0659e6e481
Alex Kiura <alex.kiura@andela.com>
2016-04-15 16:06:14 +0300

    [Chore] Refactor tests.

--- tests/test_bucketlists.py
+++ tests/test_bucketlists.py
@@ -1,12 +1,18 @@
 """This module tests CRUD operations on a user's bucketlists."""
 import json
-from test_api import ApiTestCase, db
-import nose
+from test_api import ApiTestCase
 
 
 class TestBucketLists(ApiTestCase):
+    """Test bucketlist actions."""
 
     def get_header(self):
+        """
+        Authenticate a user.
+
+        Returns:
+            request header with token
+        """
         user = {'username': 'alex', 'password': 'foobar'}
         resp_login = self.app.post('/api/v1.0/auth/login/', data=user)
         result = json.loads(resp_login.data)
@@ -14,6 +20,7 @@ class TestBucketLists(ApiTestCase):
         return {'token': token}
 
     def test_post_bucketlist(self):
+        """Test creating a new bucketlist."""
         data = {'list_name': 'Travelling'}
         resp_bucketlist = self.app.post('/api/v1.0/bucketlists/',
                                         data=data, headers=self.get_header())
@@ -24,12 +31,13 @@ class TestBucketLists(ApiTestCase):
                           'message': 'success'})
 
     def test_necessary_fields(self):
+        """Test mandatory fields."""
         resp_bucketlist = self.app.post('/api/v1.0/bucketlists/',
                                         headers=self.get_header())
-        result_bucketlist = json.loads(resp_bucketlist.data)
-        self.assertEqual(resp_bucketlist.status_code, 400)
+        self.assert400(resp_bucketlist)
 
     def test_get_bucketlists(self):
+        """Test retrieveing bucketlists."""
         self.app.post('/api/v1.0/bucketlists/', data={'list_name': 'Drinks'},
                       headers=self.get_header())
         resp_bucketlist = self.app.get('/api/v1.0/bucketlists/',
@@ -40,6 +48,7 @@ class TestBucketLists(ApiTestCase):
         self.assertEqual(result.get('list_name'), 'Drinks')
 
     def test_put_bucketlist(self):
+        """Test updating a bucketlist."""
         self.app.post('/api/v1.0/bucketlists/', data={'list_name': 'Drinks'},
                       headers=self.get_header())
         resp_bucketlist = self.app.put('/api/v1.0/bucketlists/1/',
@@ -48,6 +57,7 @@ class TestBucketLists(ApiTestCase):
         self.assertEqual(resp_bucketlist.status_code, 200)
 
     def test_delete_bucketlist(self):
+        """Test deleting a bucketlist."""
         self.app.post('/api/v1.0/bucketlists/', data={'list_name': 'Drinks'},
                       headers=self.get_header())
         resp_bucketlist = self.app.delete('/api/v1.0/bucketlists/1/',
@@ -55,6 +65,7 @@ class TestBucketLists(ApiTestCase):
         self.assertEqual(resp_bucketlist.status_code, 200)
 
     def test_bucketlist_pagination(self):
+        """Test pagination."""
         with open('tests/list_names.in', 'r') as f:
             for list_name in f.readlines():
                 self.app.post('/api/v1.0/bucketlists/',
@@ -67,6 +78,7 @@ class TestBucketLists(ApiTestCase):
         self.assertEqual(len(results), 5)
 
     def test_bucketlist_search(self):
+        """Test search bucketlists by name."""
         with open('tests/list_names.in', 'r') as f:
             for list_name in f.readlines():
                 self.app.post('/api/v1.0/bucketlists/',
@@ -78,6 +90,7 @@ class TestBucketLists(ApiTestCase):
         self.assertEqual(len(results), 1)
 
     def test_single_bucketlist(self):
+        """Test retrieving bucketlist by id."""
         data = {'list_name': 'Travelling'}
         self.app.post('/api/v1.0/bucketlists/',
                       data=data, headers=self.get_header())
@@ -86,6 +99,7 @@ class TestBucketLists(ApiTestCase):
         self.assert200(resp_bucketlist)
 
     def test_no_bucketlist(self):
+        """Test retrieving none existent bucketlists."""
         data = {'list_name': 'Travelling'}
         self.app.post('/api/v1.0/bucketlists/',
                       data=data, headers=self.get_header())
