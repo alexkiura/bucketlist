7aceeaf0708e8e1f611141cd94f0eec62379e0f4
Alex Kiura <alex.kiura@andela.com>
2016-04-16 15:45:11 +0300

    [Test] Test for token rather than authorization t Please enter the commit message for your changes. Lines starting

--- tests/test_bucketlistitems.py
+++ tests/test_bucketlistitems.py
@@ -16,8 +16,8 @@ class TestBucketListItems(ApiTestCase):
         user = {'username': 'alex', 'password': 'foobar'}
         resp_login = self.app.post('/api/v1.0/auth/login/', data=user)
         result = json.loads(resp_login.data)
-        token = result.get('token').encode('ascii')
-        return {'token': token}
+        token = result.get('authorization').encode('ascii')
+        return {'authorization': token}
 
     def add_bucketlist(self):
         """Create new bucketlist to test bucketlist item actions."""
